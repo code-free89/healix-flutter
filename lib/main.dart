af072aec5242ecc829ddfd323e8c8de173ab27d8
= <=>
2024-05-13 18:38:40 +0530

    Made an custom container for the initial pages of the app,solved the flickering issue on first launch of the app.

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: SplashScreen(),
+        home: HomePage(),
         routes: {
           '/login': (context) => UserLogin(),
           '/sign_up': (context) => UserSignUp(),
